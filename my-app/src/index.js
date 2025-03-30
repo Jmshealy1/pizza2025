26a2daefb152dd05a9c260e7afc6ab4f633ab122
Jmshealy1 <jmshealy@email.sc.edu>
2025-03-30 11:32:13 -0400

    gear pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './index.css';
-import Layout from './Layout';
-import Home from './pages/Home';
-import Blog from './pages/Blog';
-import About from './pages/About';
-import Nopage from './pages/Nopage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Guides from "./pages/Guides";
+import Gear from "./pages/Gear";
+import Community from "./pages/Community";
+import Plan from "./pages/Plan";
+import Contact from "./pages/Contact";
+import "./index.css";
 
-function App() {
-  return(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="*" element={<Nopage/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="hunting-guides" element={<Guides />} />
+        <Route path="gear-and-review" element={<Gear />} />
+        <Route path="community-hub" element={<Community />} />
+        <Route path="plan-your-trip" element={<Plan />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
