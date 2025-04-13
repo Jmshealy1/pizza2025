0741d13de92b6a99c33252e25963c32a2c86c7d1
Jmshealy1 <jmshealy@email.sc.edu>
2025-04-13 14:27:05 -0400

    gear

--- my-app/src/components/Gear.jsx
+++ my-app/src/components/Gear.jsx
@@ -1,4 +1,4 @@
-/*import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import GearItem from "../components/GearItem";
 import "../css/Gear.css";
 
